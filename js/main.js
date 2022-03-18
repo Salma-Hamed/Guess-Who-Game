4abc0ed6146edf6e7d3c54e84a1445752f2c764a
Salma-Hamed <salma.99.hamed@gmail.com>
2022-03-18 16:46:01 +0200

    Created the main project structure

--- js/main.js
+++ js/main.js
@@ -1,1 +1,5 @@
-
+function begin(){
+    var playerCard = document.getElementById('player-card')
+    var rand = Math.floor(Math.random() * 20); 
+    playerCard.innerHTML = `<img class="w-100" src="./images/ch${rand}.jpg" alt="">`
+}
